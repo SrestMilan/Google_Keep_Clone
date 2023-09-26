ec7f00703fe9321442367ffa8593473eb8301795
SrestMilan <shrestham780@gmail.com>
2023-09-26 08:07:55 +0545

    finished crud

--- script.js
+++ script.js
@@ -10,11 +10,30 @@ const addNewNote = (text = "") => {
             <button class="edit"><i class="fas fa-edit"></i></button>
             <button class="delete"><i class="fas fa-trash-alt"></i></button>
         </div>
-        <div class="main"></div>
-    <textarea ></textarea> `;
+        <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"   ></textarea> `;
 
   note.insertAdjacentHTML("afterbegin", htmlData);
   document.body.appendChild(note);
+
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const txtArea = note.querySelector("textarea");
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    txtArea.classList.toggle("hidden");
+  });
+
+  txtArea.addEventListener("change", (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+  });
 };
 
 // fire event
